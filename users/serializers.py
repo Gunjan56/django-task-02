53edd8b038f6633de07eceb178558fbf0a9c9377
Gunjan Gangwar <gangwargunjan9@gmail.com>
2024-04-26 14:11:43 +0530

    updated manager command and rechecked all apis in postman

--- users/serializers.py
+++ users/serializers.py
@@ -6,24 +6,8 @@ class UserSerializer(serializers.ModelSerializer):
         model = CustomUser
         fields = ['id', 'username', 'email', 'password', 'role']
         extra_kwargs = {'password': {'write_only': True}}
-
-
+        
     def create(self, validated_data):
-        role = validated_data.get('role')
-        if role and role.lower() != 'employee':
-            raise serializers.ValidationError("Role must be 'employee'.")
+        role = validated_data['employee']    
         user = CustomUser.objects.create_user(**validated_data)
         return user
-
-class ManagerSerializer(UserSerializer):
-    class Meta:
-        model = CustomUser
-        fields = ['id', 'username', 'email', 'password', 'role']
-        extra_kwargs = {'password': {'write_only': True}}
-
-    def create(self, validated_data):
-        if validated_data['role'].lower() != 'manager':
-            raise serializers.ValidationError('Role should be "manager".')  
-        user = CustomUser.objects.create_user(**validated_data)
-        return user
-      
--- users/urls.py
+++ users/urls.py
@@ -3,7 +3,6 @@ from . import views
 
 urlpatterns = [
     path('register/', views.UserRegistrationAPIView.as_view()),
-    path('create_manager/', views.CreateManagerAPIView.as_view()),
     path('login/', views.UserLoginAPIView.as_view()),
     path('', views.UserListCreateAPIView.as_view()),
     path('<int:pk>/', views.UserRetrieveUpdateDestroyAPIView.as_view())
